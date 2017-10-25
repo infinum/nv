6e0b42c9cf370e4104e52f13856d9ce10e965869
Andrei Zvonimir Crnkovic <andrei@infinum.co>
2017-10-25 14:27:08 +0200

    update formula

--- Formula/nv.rb
+++ Formula/nv.rb
@@ -1,8 +1,8 @@
 class Nv < Formula
   desc "Node version management"
   homepage "https://github.com/infinum/nv"
-  url "https://github.com/infinum/nv/archive/v0.0.1.tar.gz"
-  sha256 "c9fb26910909afbc79e85b68ef00080134b9e9c4ac3c2d875354264219e052d5"
+  url "https://github.com/infinum/nv/archive/v0.0.2.tar.gz"
+  sha256 "d0d611f2df87c89ff1cab34a40bc2c56661aff91c05adc6b7d0e1ae861f60790"
   head "https://github.com/infinum/nv.git"
 
   def install
